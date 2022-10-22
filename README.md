4539eaefd540003af8544c0a2993adb5a3bf716d
hazadus <hazadus7@gmail.com>
2022-10-22 15:18:44 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Blog app (Chapter 5)
+Tutorial app from ['Django for Beginners' WSV's book](https://learndjango.com/books/).
+
+## Features
+- Class-based views.
+- Tests.
+
+## References
+- [Detailed Django workflow](https://hazadus.notion.site/Django-Dev-Workflow-f197b6b9f6454e87be6dcbfa9f8f31f1)
+
+## Starting app
+```bash
+$ source .venv/bin/activate
+$ python manage.py runserver
+```
---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    author = models.ForeignKey(
+        to="auth.User",  # Reference built-in User model
+        on_delete=models.CASCADE,  # Delete all the stuff if user is deleted?
+    )  # https://docs.djangoproject.com/en/4.1/ref/models/fields/#django.db.models.CASCADE
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk': self.pk})

396ff0c28ef04acd366600e2f4fda310a43af1f5
hazadus <hazadus7@gmail.com>
2022-10-22 18:23:34 +0300

    DeleteView added.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/<int:pk>/', BlogDetailView.as_view(), name='view_post'),
     path('post/create/', BlogCreateView.as_view(), name='create_post'),
     path('post/edit/<int:pk>/', BlogUpdateView.as_view(), name='update_post'),
+    path('post/delete/<int:pk>/', BlogDeleteView.as_view(), name='delete_post'),
 ]

4346d1eb4eeb7e359aafd6aafccf8fe5f02a55af
hazadus <hazadus7@gmail.com>
2022-10-22 18:11:44 +0300

    UpdateView added.

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,14 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         """
+        Used to redirect to individual page after editing post.
+
+        One place Django uses get_absolute_url() is in the admin app. If an object defines this method,
+        the object-editing page will have a “View on site” link that will jump you directly to the object’s
+        public view, as given by get_absolute_url().
+
         Used in tests to check if the absolute URL is correct.
+
+        https://docs.djangoproject.com/en/4.1/ref/models/instances/#get-absolute-url
         """
         return reverse('view_post', kwargs={'pk': self.pk})
--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
     # NB: no '/' before 'post/...' needed!:
     path('post/<int:pk>/', BlogDetailView.as_view(), name='view_post'),
     path('post/create/', BlogCreateView.as_view(), name='create_post'),
+    path('post/edit/<int:pk>/', BlogUpdateView.as_view(), name='update_post'),
 ]
